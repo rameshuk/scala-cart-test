7130d8d35a06c3f91bbfd9cf22e448f4298912ee
Ramesh Sukka <ramesh.sukka@gmail.com>
2017-08-07 14:13:07 +0100

    Step-2 Enhanced checkout service with offers

--- src/main/scala/CheckoutService.scala
+++ src/main/scala/CheckoutService.scala
@@ -3,17 +3,39 @@ import ModelProtocol._
 
 class CheckoutService {
 
-  def calculateCostOfBasket(basket: List[Fruit]): String = {
-    val sum = basket.map(_.value).sum / 100
-    f"$sum%1.2f"
+  /*
+   * calculates cost of basket
+   * by default offers not applied to basket
+   */
+  def calculateCostOfBasket(basket: List[Fruit])(implicit applyOffer: Boolean = false): String = {
+
+    val total = {
+      if (applyOffer) applyCurrentOffers(basket) else basket
+    }.map(_.value).sum / 100
+    "%1.2f".format(total)
   }
 
+
+  //list offers can apply here
+  private def applyCurrentOffers(basket: List[Fruit]): List[Fruit] = {
+    val basketAfterApplyOffer = applyOffer(basket, Apple, _ / 2) //buy one get one offer
+    applyOffer(basketAfterApplyOffer, Orange, _ * 2 / 3) //3 for 2
+
+  }
+
+  private def applyOffer(basket: List[Fruit], prod: Fruit, apply: Double => Double): List[Fruit] = {
+    val partition = basket.partition(_ == prod)
+    val noItemsAfterApply = math.ceil(apply(partition._1.length.toDouble)).toInt
+    List.fill[Fruit](noItemsAfterApply)(prod) ::: partition._2
+  }
 }
 
 object ModelProtocol {
 
-  sealed trait Fruit{ def value: BigDecimal  }
-  case object Apple extends Fruit {override val value: BigDecimal  = 60}
-  case object Orange extends Fruit {override val value: BigDecimal  = 25}
+  sealed trait Fruit { def value: BigDecimal }
+
+  case object Apple extends Fruit { override val value: BigDecimal = 60 }
+
+  case object Orange extends Fruit { override val value: BigDecimal = 25 }
 
 }
--- src/test/scala/CheckoutServiceSpec.scala
+++ src/test/scala/CheckoutServiceSpec.scala
@@ -9,7 +9,7 @@ class CheckoutServiceSpec extends fixture.FlatSpec with Matchers {
 
   override protected def withFixture(test: OneArgTest): Outcome = test(new CheckoutService)
 
-  behavior of "CheckoutService"
+  behavior of "CheckoutService without any offers"
 
   it should "return 0.00 when checkout basket is empty" in {
     s => s.calculateCostOfBasket(List.empty[Fruit]) should equal("0.00")
@@ -26,6 +26,23 @@ class CheckoutServiceSpec extends fixture.FlatSpec with Matchers {
     s => s.calculateCostOfBasket(createBasket(50, 70)) should equal("47.50")
   }
 
+  behavior of "CheckoutService with  offers"
+
+  it should "return 4.10 when 12 Apples and 2 Oranges with buy one and get one offer on Apples" in {
+    s => s.calculateCostOfBasket(createBasket(12, 2))(applyOffer = true) should equal("4.10")
+  }
+
+  it should "return 0.85 when 2 Apples and 1 Oranges with buy one and get one offer on Apples" in {
+    s => s.calculateCostOfBasket(createBasket(2, 1))(applyOffer = true) should equal("0.85")
+  }
+
+  it should "return 9.25 when 55 Oranges with three for two offer on oranges" in {
+    s => s.calculateCostOfBasket(createBasket(0, 55))(applyOffer = true) should equal("9.25") //cost for 37 oranges
+  }
+
+  it should "return 6.10 when 2 Apples and 32 Oranges with 2 for 1 apples and three for two offer on oranges" in {
+    s => s.calculateCostOfBasket(createBasket(2, 32))(applyOffer = true) should equal("6.10") //cost for 1 apple, 22 oranges
+  }
 
   private def createBasket(apples: Int, oranges: Int): List[Fruit] = {
     List.fill(apples)(Apple) ::: List.fill(oranges)(Orange)
